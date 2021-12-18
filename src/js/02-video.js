2c7b10e8e74e0197acb0170c7dbf6d4bf4a2d7ea
Vitaliy <networkvetalius@gmail.com>
2021-12-18 22:37:29 +0200

    02-video done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function(e) {
+  localStorage.setItem("videoplayer-current-time", e.seconds);
+}, 1000));
+const currentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currentTime).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+ 
+
+
+
 
