7dcaaf12d1be4b57972f739a4514b1b1a5fc1590
Vitaliy <networkvetalius@gmail.com>
2021-12-18 20:03:40 +0200

    01-gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+}
+
+refs.gallery.addEventListener('click', onImageClick)
+
+function onImageClick(event) {
+  event.preventDefault();
+  const isClickOnGalleryImage = event.target.classList.contains('gallery__image');
+  if (!isClickOnGalleryImage) {
+    return;
+  }
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(item => {
+  return  `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}"  />
+</a>`
+}).join('');
+}
+
+refs.gallery.innerHTML = galleryMarkup;
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
